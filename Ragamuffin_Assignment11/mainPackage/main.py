798fd6a3294ab1283613f532c4de1afb4d2cff92
ezishr <voquyphiqn2004@gmail.com>
2025-04-16 13:14:46 -0400

    test zip code api

--- Ragamuffin_Assignment11/mainPackage/main.py
+++ Ragamuffin_Assignment11/mainPackage/main.py
@@ -8,7 +8,10 @@ data = gross_price_two_decimals(data)
 
 data = detect_pepsi(data, header)
 
+zip_code = request_api_zipcode(city = 'Cincinnati', state_name = 'Ohio')
+print(zip_code)
 
+print(data[0])
 
 
 # NEED: function to get rows missing zip code
